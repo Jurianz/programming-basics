7d1b7f7629bf72b08ddf956adcb4d5bc25b7debc
Jurianz <42801635+Jurianz@users.noreply.github.com>
2018-09-26 16:49:10 +0200

    opdrachten les 5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 //De eerste opdracht:variables
-const getal = 6
+/*const getal = 6
 if (getal % 2 == 0) {
     console.log('even');
 } else {
@@ -20,7 +20,7 @@ if (a == b) {
 
 //De tweede opdracht:Conditionals
 
-let cijfer = 8;
+/*slet cijfer = 8;
 if (cijfer < 6) {
     console.log('onvoldoende');
 } else if (cijfer >= 6 && cijfer <= 7) {
@@ -31,7 +31,7 @@ if (cijfer < 6) {
     console.log('uitmuntend');
 }
 
-let tekst; 
+let tekst;
 switch (cijfer) {
     case 1:
     case 2:
@@ -63,36 +63,84 @@ inTrain = false;
 
 // Opdrachten over loops
 // Reeks met getallen deelbaar of niet deelbaar
-x=1;
-while (x<=20) {
-    if (x % 4 ===0) {
-        console.log(x,'deelbaar door 4');
-    }else{
+/*x = 1;
+while (x <= 20) {
+    if (x % 4 === 0) {
+        console.log(x, 'deelbaar door 4');
+    } else {
         console.log(x, 'is niet deelbaar door 4');
     }
-    x=x+1;
+    x = x + 1;
 
     // Fibonacci reeks
-let firstNumber = 0;
-let secondNumber = 1;
-let firstAndSecond = firstNumber + secondNumber;
+    /*let firstNumber = 0;
+    let secondNumber = 1;
+    let firstAndSecond = firstNumber + secondNumber;
 
-while(firstAndSecond <1000){
-    console.log(firstAndSecond);
-    secondNumber = firstNumber;
-    firstNumber = firstAndSecond;
-    firstAndSecond = firstNumber + secondNumber;
-}
+    while (firstAndSecond < 1000) {
+        console.log(firstAndSecond);
+        secondNumber = firstNumber;
+        firstNumber = firstAndSecond;
+        firstAndSecond = firstNumber + secondNumber;
+    }*/
 
 // array
-let Getallen = [2, 4, 8, 9, 12, 13];
+/*let Getallen = [2, 4, 8, 9, 12, 13];
 let totalOffAll = 0;
 
-for(z=0;z<Getallen.length;z++){
+for (z = 0; z < Getallen.length; z++) {
     console.log(Getallen[z]);
     totalOffAll = totalOffAll + Getallen[z];
 }
 console.log(totalOffAll);
+}*/
+
+// fibonaccisequence in function
+function fibonacci(startNum, maxNum) {
+    let secondNumber = 0;
+    let firstAndSecond = startNum + secondNumber;
+    while (firstAndSecond < maxNum) {
+        console.log(firstAndSecond);
+        secondNumber = startNum;
+        startNum = firstAndSecond;
+        firstAndSecond = startNum + secondNumber;
+    }
 }
+// dit geeft de parameters aan van de functie
+fibonacci(1, 900);
+
+/*function countdown(seconds) {
+    if (seconds) {
+        console.log(seconds + ' seconden te gaan...');
+        setTimeout(countdown, 1000, --seconds);
+    } else {
+        var d = new Date;
+        var currentYear = d.getFullYear();
+        console.log(`Happy ${currentYear}!`);
+    }
+}
+countdown(10)*/
+// Functie expressie en declaratie
+function declar(){
+   a =12;
+   b =13;
+   console.log(`${a},${b}`);
+   var a;
+   var b;
+}
+declar();
+
+//expressie functie
+function expression(){
+    var y = 12;
+
+    var calc= z + y;
+    console.log(calc);
+    var z = 13;
+}
+expression()
+// resultaat geef NaN
+
+
 
 
